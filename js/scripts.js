b2341f2f8fa135cd21ae6e3c13c640dbdb6efcf3
TristanLobaugh <ttrader_atl@yahoo.com>
2016-03-22 14:37:22 -0400

    added footables
    added footables so can sort and manage the information on the table

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 	$(document).ready(function(){
+
+
 		$("#ticker-search").submit(function(){
 			var ticker = $("#symbol").val();
 			var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("' + ticker + '")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
@@ -15,6 +17,9 @@
 						newHTML = updateTable(stockInfo);
 					}
 					$("#ticker-body").html(newHTML);
+					$(function () {
+						$('.footable').footable();
+					});
 				});
 			event.preventDefault();
 		});
